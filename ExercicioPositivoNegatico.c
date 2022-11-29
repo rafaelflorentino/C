4cb12ac6bfa1db7fb0913dd2f772f0b4911792e6
Rafael <enterrafael@gmail.com>
2022-11-29 18:51:10 -0300

    novo exercicio

---
+++ ExercicioPositivoNegatico.c
@@ -0,0 +1,31 @@
+
+/*
+ Lista2_Ex1
+ Objetivo : Mostre se um numero é positivo, negativo ou nulo (zero).
+ Entrada : Um número
+ Saida : Positivo , negativo ou nulo.
+ Aluno : Rafael Florentino.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int num1;
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+    fflush(stdin);
+
+    if (num1 == 0)
+        printf("O numero: %d e nulo (zero).", num1);
+
+    if (num1 < 0)
+        printf("O numero: %d e negativo.", num1);
+
+    if (num1 > 0)
+        printf("O numero: %d e positivo.", num1);
+    getch();
+    
+    return 0;
+}
