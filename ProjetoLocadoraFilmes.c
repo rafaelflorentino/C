999a6be05dbecf3c7920a21988c3c9c989a7c62f
Rafael <enterrafael@gmail.com>
2022-12-23 23:37:54 -0300

    Projeto Locadora, reserva, maisculo, minusculo

--- ProjetoLocadoraFilmes.c
+++ ProjetoLocadoraFilmes.c
@@ -12,7 +12,7 @@
 
 struct estrutura
 {
-  int codigo[100], quantidadeDisponivel[100], totalCadastrado, quantidadeSuspense;
+  int codigo[100], quantidadeDisponivel[100], totalCadastrado, quantidadeSuspense, reservados[100];
   char nome[100][100], genero[100][100];
 
 } Filme;
@@ -23,6 +23,9 @@ void verificaSuspence(char generoFilme[100]);
 void MenuConsultaFilme();
 void consultarNome();
 void consultarCodigo();
+void efetuarReserva();
+void cadastrarMais();
+void converte(char r1[], char r2[], char r3[]);
 
 int main()
 {
@@ -30,7 +33,7 @@ int main()
   printf("\n                 ************ Locadora Favorita ****************\n");
   printf("\n                     ********** Seja Bem Vindo ***********\n");
   cadastrarFilmes();
-  
+
   return 0;
 }
 
@@ -70,7 +73,7 @@ void listarFilmes()
   printf("\n                        ** Lista de Filmes **\n\n");
   for (i = 0; i < Filme.totalCadastrado; i++)
   {
-    printf("Filme %d. Codigo: %d; Nome: %s; Genero: %s; Quantidade: %d; \n", i + 1, Filme.codigo[i], Filme.nome[i], Filme.genero[i], Filme.quantidadeDisponivel[i]);
+    printf("Filme %d. Codigo: %d; Nome: %s; Genero: %s; Quantidade: %d; Reservados: %d \n", i + 1, Filme.codigo[i], Filme.nome[i], Filme.genero[i], Filme.quantidadeDisponivel[i], Filme.reservados[i]);
   }
   printf("\n                     **Fim da Lista de Filmes**\n\n");
   system("pause");
@@ -80,11 +83,12 @@ void listarFilmes()
 void MenuPrincipal()
 {
   int opcaoMenu;
-  printf("\n                               ****Menu Principal***");
+  printf("\n                               **** Menu Principal ***");
   printf("\n*** Digite 1 Para Consultar um filme. ***");
   printf("\n*** Digite 2 Para reservar um filme. ***");
   printf("\n*** Digite 3 para ver a Quantidade de filmes de Suspense. ***");
-  printf("\n*** Digite 4 Para sair. ***\n");
+  printf("\n*** Digite 4 Para Cadastrar mais Filmes. ***");
+  printf("\n*** Digite 5 Para sair. ***\n");
   printf("\nDigite o numero da opcao: ");
   scanf("%d", &opcaoMenu);
   fflush(stdin);
@@ -95,7 +99,9 @@ void MenuPrincipal()
     MenuConsultaFilme();
     break;
   case 2:
-    printf("\nReservarFilme() fazer");
+    efetuarReserva();
+    system("pause");
+    MenuPrincipal();
     break;
   case 3:
     printf("\nQuantidade de filmes de Suspense: %d", Filme.quantidadeSuspense);
@@ -104,18 +110,22 @@ void MenuPrincipal()
     MenuPrincipal();
     break;
   case 4:
-    printf("\nPrograma Encerrado.\n");
+    cadastrarMais();
     break;
+  case 5:
+    printf("\nPrograma Encerrado.\n");
+    break;      
   }
   printf("\n");
   return 0;
 }
 
 void verificaSuspence(char generoFilme[100])
-{
+{  
   int ret = strcmp("suspense", generoFilme);
   int ret2 = strcmp("Suspense", generoFilme);
   int ret3 = strcmp("SUSPENSE", generoFilme);
+
   if (ret == 0 || ret2 == 0 || ret3 == 0)
   {
     Filme.quantidadeSuspense++;
@@ -129,7 +139,7 @@ void MenuConsultaFilme()
   printf("\n*** Digite 1 para Consultar filme pelo nome ***  ");
   printf("\n*** Digite 2 para Consultar filme pelo codigo ***  ");
   printf("\n*** Digite 3 para Listar todos os filmes cadastrados **** ");
-  printf("\n*** Digite 4 para encerrar a Consulta **** ");
+  printf("\n*** Digite 4 para encerrar a Consulta e voltar ao Menu Principal **** ");
   printf("\n\nDigite o numero da opcao: ");
   scanf("%d", &opcaoMenu);
   fflush(stdin);
@@ -200,3 +210,100 @@ void consultarCodigo()
   temp = 0;
   printf("\n");
 }
+
+void efetuarReserva()
+{ 
+  int codigoFilme, temp = 0;
+  printf("\n                        ********* Area De Reserva ***********\n ");
+  printf("\nDigite o Codigo do Filme: ");
+  scanf("%d", &codigoFilme);
+  fflush(stdin);
+  for (int i = 0; i < Filme.totalCadastrado; i++)
+  {
+    if (codigoFilme == Filme.codigo[i])
+    {
+      temp++;
+      if (Filme.quantidadeDisponivel[i] > Filme.reservados[i])
+      {
+        Filme.reservados[i]++;
+        printf("\nFilme Reservado com Sucesso, Codigo: %d; Nome: %s; Quantidade: %d; Reservados: %d;\n", Filme.codigo[i], Filme.nome[i], Filme.quantidadeDisponivel[i], Filme.reservados[i]);        
+      }
+      else
+      {
+        printf("\nTodas as unidades desse Filme ja foram Alugadas ou Reservadas. Codigo: %d; Nome: %s; Quantidade: %d; Reservados: %d;\n", Filme.codigo[i], Filme.nome[i], Filme.quantidadeDisponivel[i], Filme.reservados[i]);
+      }
+    }
+  }
+  if (temp == 0)
+  {
+    printf("\nFilme Inexistente, nao foi cadastrado, Codigo Digitado: %d\n", codigoFilme);
+  }
+  temp = 0;
+  printf("\n");
+}
+
+void cadastrarMais()
+{
+  int i, j, codigo = 1;
+  int ultimoFilme;
+  ultimoFilme = Filme.totalCadastrado ;
+
+  for (i = ultimoFilme; codigo != 0; i++)
+  {
+    printf("\n                        ****** Cadastrar Novo Filme ********\n\n");
+    printf("Digite 0 para SAIR ou qualquer tecla para Cadastrar novo Filme: ");
+    scanf("%d", &codigo);
+    fflush(stdin);
+
+    if (codigo != 0)
+    {
+      printf("\n                        ***** Filme %d *****\n\n", i + 1);
+      printf("Digite o codigo do Filme %d: ", i + 1);
+      scanf("%d", &Filme.codigo[i]);
+      fflush(stdin);
+      printf("Digite o nome do Filme %d: ", i + 1);
+      gets(Filme.nome[i]);
+      fflush(stdin);
+      printf("Digite o genero do Filme %d (Suspense, Terror, Romance, Comedia): ", i + 1);
+      gets(Filme.genero[i]);
+      verificaSuspence(Filme.genero[i]);
+      fflush(stdin);
+      printf("Digite a quantidade do Filme %d: ", i + 1);
+      scanf("%d", &Filme.quantidadeDisponivel[i]);
+      fflush(stdin);
+      Filme.totalCadastrado++;
+      printf("\n ");
+      system("pause");
+      system("cls");
+    }
+  }
+  MenuPrincipal();
+}
+// coverte em 2 strings uma maisucula e a outra minuscula
+void converte(char r1[], char r2[], char r3[]){  
+    printf("String original: %s\n", r1);
+    minusculo(r1, r2);
+    printf("String Minuscula: %s\n", r2);
+    maiusculo(r1, r3);
+    printf("String Maiuscula: %s\n", r3);
+    printf("Final da conversao. \n");
+}
+// Procedimento que converte uma string para maiúsculo.
+void maiusculo(char s1[], char s2[]){
+    int i = 0;
+    while(s1[i] != '\0'){
+        s2[i] = toupper(s1[i]);
+        i++;
+    }
+    s2[i] = '\0'; // caracter que indica o fim da string.
+}
+// Procedimento que converte uma string para minúsculo.
+void minusculo(char s1[], char s2[]){
+    int i = 0;
+    while(s1[i] != '\0'){
+        s2[i] = tolower(s1[i]);
+        i++;
+    }
+    s2[i] = '\0'; // Caracter que indica o fim da string.
+}
+
