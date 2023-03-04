7547ec64709765ce122d0542dc644593ca97ec26
Rafael <enterrafael@gmail.com>
2023-03-04 16:15:13 -0300

    Percorrendo arvores

--- exercicios/EDA/ExercicioArvoreBinaria.c
+++ exercicios/EDA/ExercicioArvoreBinaria.c
@@ -26,9 +26,12 @@ int vai_ter_filho_a_esquerda(float Elemento)
     fflush(stdin);
     printf("\nDeseja filho a ESQUERDA de %d ? 'S' para Sim ou 'N' para Nao : ", Elemento);
     Resp = toupper(getchar());
-    if (Resp == 'S'){
+    if (Resp == 'S')
+    {
         return 1;
-    }else{
+    }
+    else
+    {
         return 0;
     }
 }
@@ -39,9 +42,12 @@ int vai_ter_filho_a_direita(float Elemento)
     fflush(stdin);
     printf("\nDeseja filho a DIREITA de %d ? 'S' para Sim ou 'N' para Nao : ", Elemento);
     Resp = toupper(getchar());
-    if (Resp == 'S'){
+    if (Resp == 'S')
+    {
         return 1;
-    }else{
+    }
+    else
+    {
         return 0;
     }
 }
@@ -51,13 +57,20 @@ void le_arvore(no *raiz)
 {
     *raiz = (no)malloc(sizeof(cel));
     (*raiz)->elem = le_conteudo();
-    if (vai_ter_filho_a_esquerda((*raiz)->elem)){
+    if (vai_ter_filho_a_esquerda((*raiz)->elem))
+    {
         le_arvore(&((*raiz)->esq));
-    }else{
+    }
+    else
+    {
         (*raiz)->esq = NULL;
-    }if (vai_ter_filho_a_direita((*raiz)->elem)){
+    }
+    if (vai_ter_filho_a_direita((*raiz)->elem))
+    {
         le_arvore(&(*raiz)->dir);
-    }else{
+    }
+    else
+    {
         (*raiz)->dir = NULL;
     }
 }
@@ -86,3 +99,87 @@ int main()
     system("PAUSE");
     return 0;
 }
+/*
+// inserção na arvore
+insere(arvore r, no *novo)
+{
+    if (r == NULL) // quando acha o campo vazio null, insere nessa posição
+        return novo;
+    if (r->chave > novo->chave)
+        r->esq = insere(r->esq, novo);
+    else
+        r->dir = insere(r->dir, novo);
+    return r;
+}
+// busca na arvore
+busca(arvore r, int k)
+{
+    if (r == NULL || r->chave == k) // retorna nulo caso não ache o número
+        return r;
+    if (r->chave > k)
+        return busca(r->esq, k);
+    else
+        return busca(r->dir, k);
+}
+// Remover o Elemento Raiz da Arvore
+// Recebe uma árvore não vazia r.
+// Remove a raiz da árvore e rearranja a árvore de modo que ela continue sendo
+// de busca. Devolve o endereço da nova raiz.
+removeraiz(arvore r)
+{
+    no *p, *q;
+    if (r->esq == NULL)
+    {
+        q = r->dir;
+        free(r);
+        return q;
+    }
+    p = r;
+    q = r->esq;
+    while (q->dir != NULL)
+    {
+        p = q;
+        q = q->dir;
+    }
+    // q é nó anterior a r na ordem e-r-d
+    // p é pai de q
+    if (p != r)
+    {
+        p->dir = q->esq;
+        q->esq = r->esq;
+    }
+    q->dir = r->dir;
+    free(r);
+    return q;
+}
+// Travessia de Arvore
+// em-ordem
+    // Esquerda, Raiz, Direita
+void ListarEmOrdem(PNodoArv T){
+    if(T != null ){
+        ListarEmOrdem(T-> Esquerda);
+        printf(T -> Elemento);
+        ListarEmOrdem(T-> Direita);
+    }
+}
+
+// Pre-ordem
+    // Raiz, Esquerda, Direita
+void ListarPreOrdem(PNodoArv T){
+    if(T != null ){
+        printf(T -> Elemento);
+        ListarPreOrdem(T-> Esquerda);
+        ListarPreOrdem(T-> Direita);
+    }
+}
+// Pós ordem
+// Esquerda, Dierita Raiz
+void ListarPosOrdem(PNodoArv T){
+    if(T != null ){
+        ListarPosOrdem(T-> Esquerda);
+        ListarPosOrdem(T-> Direita);
+        printf(T -> Elemento);
+    }
+}
+*/
+
